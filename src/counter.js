b39304c93f01ee39fb647827e66dfaaeac133787
fawazbayureksa <fawwazbayureksa@gmail.com>
2022-03-27 09:04:26 +0800

    counter app

---
+++ src/counter.js
@@ -0,0 +1,17 @@
+import React, { Component , useState} from "react";
+
+function Counter(){  
+    
+    
+const [count, setCount] = useState(0)
+        return (
+            <div>
+                <button onClick={() => setCount(count-1)}>-</button>
+                <p>{count}</p>
+                <button onClick={() => setCount(count+1) }>+</button>
+            </div>
+        )
+}
+
+
+export default Counter;
